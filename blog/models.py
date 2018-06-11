3a9c35373970c4eb80ac83b435ed14aee17fe60a
Raj Poru <rajporu@gmail.com>
2018-06-10 21:47:09 -0700

    Added bootstrap and jobs are shpwing up

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+# Create a blog model
+class Blog(models.Model):
+    # title
+    title = models.CharField(max_length=255)
+
+    # pub_date
+    pub_date = models.DateTimeField()
+
+    # body
+    body = models.TextField()
+
+    # image
+    image = models.ImageField(upload_to='images/')
+
+# Add the blog app to the settings
+
+# Create a migration
+# python manage.py makemigrations
+
+# Migrate
+# python manage.py migrate
+
+# Add to the admin
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from .models import Job
+
 # Create your views here.
+def home(request):
+    # Grab everything from database
+    jobs = Job.objects
+    return render(request, 'jobs/home.html', {'jobs': jobs})
